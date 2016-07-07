a519449ffa52d39f4ab0ed75be00dfe40f3330c0
Stefan Hornburg (Racke) <racke@linuxia.de>
2016-07-07 18:58:13 +0200

    Add more credits for SysPete to main POD.

--- lib/Dancer2/Session/DBIC.pm
+++ lib/Dancer2/Session/DBIC.pm
@@ -459,7 +459,8 @@ Based on code from L<Dancer::Session::DBI> written by James Aitken
 and code from L<Dancer::Plugin::DBIC> written by Naveed Massjouni.
 
 Peter Mottram, support for JSON, YAML, Sereal and custom
-serializers, GH #8, #9, #11, #12.
+serializers, GH #8, #9, #11, #12. Also for adding _change_id
+method and accompanying tests.
 
 Rory Zweistra, GH #9.
 
